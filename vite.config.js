e46021d00a01d4ab443900e4462a71738c2db9b3
Faizan ALi <faizandevx@gmail.com>
2026-01-06 22:04:22 +0500

    Fix Vite build for Laravel Vue app and generate production assets

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'], // main entry file
+            input: 'resources/js/app.js',
             refresh: true,
         }),
         vue(),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
 });
